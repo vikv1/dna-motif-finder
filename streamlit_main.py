cc9dc15f67b5ccd34d742379250ca2830ccf9389
Vikrant Verma <87392159+crayzeerr@users.noreply.github.com>
2024-02-28 18:33:25 -0800

    first commit

---
+++ streamlit_main.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import test
+
+st.title("hi")
---
+++ test.py
@@ -0,0 +1,19 @@
+import suffixtree
+import SuffixTrie as trie
+import time
+import random
+
+characters = ['A', 'C', 'G', 'T']
+
+# Generate 30 sequences each around 100,000 characters long
+sequences = [''.join(random.choice(characters) for _ in range(10000000)) for _ in range(1)]
+
+
+seconds = open("seconds.txt", "w")
+for seq in sequences:
+   start = time.time()
+   tree, pst = suffixtree.build(seq, regularize=True)
+   end = time.time()
+   seconds.write(str(end - start) + "\n")
+
+print("done")
